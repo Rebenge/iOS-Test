652c4096ee3483c4bd78a17dc35bbae3a9620c61
Rebenge <ben.wordsworth@xtcarts.com>
2014-08-22 12:16:30 +0100

    Initial Commit

---
+++ iOS-Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS-Test
+//
+//  Created by Benjamin Wordsworth on 22/08/2014.
+//  Copyright (c) 2014 Benjamin Wordsworth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS-Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS-Test
+//
+//  Created by Benjamin Wordsworth on 22/08/2014.
+//  Copyright (c) 2014 Benjamin Wordsworth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
